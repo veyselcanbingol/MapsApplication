064c5a49ec1681c8d81492ad0018d202e565b139
Veysel Can Bingöl <veyselcanbingol@gmail.com>
2022-06-04 10:48:23 +0300

    Initial Commit

---
+++ MapsApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapsApplication
+//
+//  Created by Veysel Can Bingöl on 4.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
